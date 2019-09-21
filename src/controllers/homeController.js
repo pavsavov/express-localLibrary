872bde57fd5d8de491aaeee7eeb2e3d4da276fc6
Pavel <pav.savov@gmail.com>
2019-09-21 22:35:11 +0300

    Homecontroller data logic implemented to render to view.Implemented index view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,8 +5,22 @@ const BookInstance = require('../models/bookinstance');
 
 const async = require('async');
 
-async.parallel()
-
-exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
-};
+async.parallel({
+    bookCount: function (callback) {
+        BookInstance.countDocuments({}, callback);
+    },
+    authorCount: function (callback) {
+        Author.countDocuments({}, callback);
+    },
+    bookInstanceAvailableCount: function (callback) {
+        BookInstance.countDocuments({ status: 'Available' }, callback);
+    },
+    bookInstanceCount: function (callback) {
+        BookInstance.countDocuments({}, callback);
+    },
+    genreCount: function (callback) {
+        Genre.countDocuments({}, callback);
+    }
+}, function (err, result) {
+    res.render('index', { title: 'Local Library Home', error: err, data: result });
+});
