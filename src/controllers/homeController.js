47b46555c39a3285da02fbc33da4b62fb642aded
Pavel <pav.savov@gmail.com>
2019-09-21 15:31:39 +0300

    async modul added.Implemented a base layout page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const Books = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+
+const async = require('async');
+
+async.parallel()
+
 exports.index = function(req, res) {
     res.send('NOT IMPLEMENTED: Site Home Page');
 };
